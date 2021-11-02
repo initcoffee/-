d166747c37a349a84e4d756716b9fd664edac577
initcoffee <info@initcoffee.com>
2021-11-02 17:53:48 +0800

    mobile viewport test
    use of constant --appHeight instead of var --vh

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 
-  // calculate vh
-  window.addEventListener('resize', () => {
-    document.querySelector(':root').style
-      .setProperty('--vh', window.innerHeight / 100 + 'px');
-  });
-  // end of calculate vh
+  // // calculate vh
+  // window.addEventListener('resize', () => {
+  //   document.querySelector(':root').style
+  //     .setProperty('--vh', window.innerHeight / 100 + 'px');
+  // });
+  // // end of calculate vh
+  const appHeight = () => {
+      const doc = document.documentElement
+      doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+  }
+  window.addEventListener('resize', appHeight)
+  appHeight()
