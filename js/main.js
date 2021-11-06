c06e3a3a63fe0157eb3b5f8de6972d7d2c782bbc
initcoffee <info@initcoffee.com>
2021-11-06 22:04:35 +0800

    added ready func

--- js/main.js
+++ js/main.js
@@ -6,36 +6,37 @@
 // // end of calculate vh
 
 
-
-
-const appHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', appHeight)
-appHeight()
-
-
-
-
 function gotoMain() {
   document.getElementById('main-body-container').scrollIntoView();
 }
-//
-// window.addEventListener('scroll', () => {
-//   const scrolled = window.scrollY / 2;
-//   let root = document.documentElement;
-//   root.style.setProperty('--top-container-translate', scrolled + 'px');
-//
-// })
 
 
-$(window).scroll(function() {
-  var scrolled = $(window).scrollTop()/4.5;
-  // document.getElementById('top_container-fluid').style.transform ='0px,'
+$( document ).ready(function() {
+  const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+  }
+  window.addEventListener('resize', appHeight)
+  appHeight()
 
 
-  let root = document.documentElement;
-  root.style.setProperty('--top-container-translate', scrolled + 'px');
+  //
+  // window.addEventListener('scroll', () => {
+  //   const scrolled = window.scrollY / 2;
+  //   let root = document.documentElement;
+  //   root.style.setProperty('--top-container-translate', scrolled + 'px');
+  //
+  // })
 
-})
+
+  $(window).scroll(function() {
+    var scrolled = $(window).scrollTop()/4.5;
+    // document.getElementById('top_container-fluid').style.transform ='0px,'
+
+
+    let root = document.documentElement;
+    root.style.setProperty('--top-container-translate', scrolled + 'px');
+
+  });
+
+});
