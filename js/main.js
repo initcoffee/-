965e3488c2deb85ce757567e151ee1601d9c7a25
initcoffee <info@initcoffee.com>
2021-11-06 21:03:31 +0800

    added slow moving layer for top container

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
+// // calculate vh
+// window.addEventListener('resize', () => {
+//   document.querySelector(':root').style
+//     .setProperty('--vh', window.innerHeight / 100 + 'px');
+// });
+// // end of calculate vh
+const appHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
 
-  // // calculate vh
-  // window.addEventListener('resize', () => {
-  //   document.querySelector(':root').style
-  //     .setProperty('--vh', window.innerHeight / 100 + 'px');
-  // });
-  // // end of calculate vh
-  const appHeight = () => {
-      const doc = document.documentElement
-      doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-  }
-  window.addEventListener('resize', appHeight)
-  appHeight()
-
-
-
-document.getElementById('main-body-container').scrollIntoView()
 
 
 
 function gotoMain() {
   document.getElementById('main-body-container').scrollIntoView();
 }
+
+window.addEventListener('scroll', () => {
+  const scrolled = window.scrollY/2;
+  let root = document.documentElement;
+    root.style.setProperty('--top-container-translate', scrolled + 'px');
+
+  console.log(scrolled);
+})
