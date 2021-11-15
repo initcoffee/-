739f1f830a5bc1e1e9c4c1a95820b065fc5cff6c
initcoffee <info@initcoffee.com>
2021-11-15 16:03:33 +0800

    test

--- js/main.js
+++ js/main.js
@@ -18,10 +18,71 @@ function cancel() {
 
 $(document).ready(function() {
 
-  $(window).resize(function() {
-    $(content).css('height', $(window).height() - $(nav).height());
-  });
+  
+  var JumpFix = {
 
+      fixItems: $('*[data-jump-fix]'), // Get all items with the data attribute.
+      isScrolling: false,
+      timeoutScroll: null,
+
+      // This sets 'isScrolling' to true for a certain amount of time.
+      setScrollingStatus: function () {
+          if (JumpFix.timeoutScroll) {
+              clearTimeout(JumpFix.timeoutScroll);
+          }
+          JumpFix.isScrolling = true;
+          JumpFix.timeoutScroll = setTimeout(function () {
+              JumpFix.isScrolling = false;
+          }, 100);
+      },
+
+      // Save actual heights
+      saveHeights: function () {
+          JumpFix.fixItems.each(function (i) {
+              JumpFix.fixItems[i].savedHeight = $(this).height();
+          })
+      },
+
+      // window and document actions here
+      bindUIActions: function () {
+
+          // Set isScrolling when the user scrolls or swipes.
+          $(window).scroll(function () {
+              if (JumpFix.isScrolling === false) {
+                  JumpFix.saveHeights();
+              }
+              JumpFix.setScrollingStatus();
+          });
+
+          $(window).on({
+              'touchmove': function () {
+                  if (JumpFix.isScrolling === false) {
+                      JumpFix.saveHeights();
+                  }
+                  JumpFix.setScrollingStatus();
+              }
+          });
+
+          // On resize: if user is scrolling use the saved height.
+          // if user is NOT scrolling save the new nativ height after resize.
+          $(window).resize(function () {
+              if (JumpFix.isScrolling === true) {
+                  JumpFix.fixItems.each(function (i) {
+                      $(this).height(JumpFix.fixItems[i].savedHeight);
+                  })
+              } else {
+                  JumpFix.fixItems.each(function () {
+                      $(this).css('height', '');
+                  });
+              }
+          });
+      },
+
+      init: function () {
+          this.bindUIActions();
+      }
+
+  };
 
 
   // const appHeight = () => {
